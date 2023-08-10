2b2dffcfdab0a8100c2c69e060fafa7e29d59431
Varya <mameail@mail.ru>
2023-08-10 23:13:18 +0500

    zadacha8-1

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -10,7 +10,7 @@ System.Console.WriteLine( "Введите второе число" );
 int num2 = Convert.ToInt32(Console.ReadLine() );
 System.Console.WriteLine( "Введите третье число" );
 int num3 = Convert.ToInt32(Console.ReadLine() );
-if (num1 > num2 && num1 > num3 || num2 > num1 && num2 > num3 ||num3 > num1 && num3 > num2)
+if (num1 > num2 && num1 > num3)
 {
     System.Console.WriteLine($"Наибольшее число {num1} ");
 }
