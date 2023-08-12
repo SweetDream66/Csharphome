3867302577bc78af367e51efffe5044f5caf6cb0
Varya <mameail@mail.ru>
2023-08-12 20:15:46 +0500

    zadacha15

---
+++ HOMEwork1/zadacha13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine() );
+int digit = num % 10;
+if (num > 99)
+  System.Console.WriteLine(digit);
+else
+  System.Console.WriteLine("Третьей цифры нет" );
+
+
+// int num = WriteLine("Введите число ");
+// int count = num.ToString().Length;
+// Console.Write(MakeArray(num, count));
+// int WriteLine(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int MakeArray(int a, int b)
+// {
+// int result = 0;
+//     if (b < 3)
+//     {
+//         Console.Write("Третьей цифры нет ");
+//     }
+//     else
+//     {
+//         int c = 1;
+//         for (int i = b; i > 3; i--)
+//         {
+//             c = c * 10;
+//         }
+
+//         result = (a / c) % 10;
+//     }
+// return result;
+// }
---
+++ HOMEwork1/zadacha15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите цифру обозначающую день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7 ) 
+    System.Console.WriteLine($"да");
+else
+    System.Console.WriteLine($"нет");
