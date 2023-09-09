24cbdc3f2efed95312a30c3b48a77c453b2d1454
Varya <mameail@mail.ru>
2023-09-09 10:40:10 +0500

    38zadachan

--- zadacha34/Program.cs
+++ zadacha34/Program.cs
@@ -49,8 +49,6 @@ for (int i = 1; i < numbers.Length; i += 2)
 }
 Console.Write(sumNumbersEvenIndex);
 
-
-
 void FillArrayRandomNumbers(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -68,3 +66,42 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+// double[] numbers = new double[5];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Массив вещественных чисел: ");
+// PrintArray(numbers);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > max)
+        
+//             max = numbers[i];
+        
+//     if (numbers[i] < min)
+        
+//             min = numbers[i];
+        
+// }
+// Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+// void FillArrayRandomNumbers(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+        
+//             numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        
+// }
+// void PrintArray(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+        
+//             Console.Write(numbers[i] + " ");
+        
+//     Console.WriteLine();
+// }
